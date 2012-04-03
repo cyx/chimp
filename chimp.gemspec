0988cc13769564716de4fefea111cec019644f13
Cyril David <cyx.ucron@gmail.com>
2012-04-03 17:20:18 +0800

    Initial commit.

---
+++ chimp.gemspec
@@ -0,0 +1,21 @@
+Gem::Specification.new do |s|
+  s.name              = "chimp"
+  s.version           = "0.0.1"
+  s.summary           = "Lightweight mailchimp client"
+  s.description       = "For those who want something nimble."
+  s.authors           = ["Cyril David"]
+  s.email             = ["me@cyrildavid.com"]
+  s.homepage          = "http://github.com/cyx/chimp"
+
+  s.files = Dir[
+    "LICENSE",
+    "README",
+    "rakefile",
+    "lib/**/*.rb",
+    "*.gemspec",
+    "test/*.*"
+  ]
+
+  s.add_development_dependency "dep"
+  s.add_development_dependency "cutest"
+end
---
+++ test/chimp.rb
@@ -0,0 +1,49 @@
+require_relative "../lib/chimp"
+
+test "connect" do
+  chimp = Chimp.connect("foo-us1")
+
+  assert_equal("https://us1.api.mailchimp.com/1.3/?method=", chimp.url)
+  assert_equal({ apikey: "foo-us1" }, chimp.data)
+end
+
+scope do
+  # minimalistic mocking :-)
+  class Chimp
+    def curl(url, options)
+      $curl = "curl #{options} #{url}"
+
+      return $curl_result if $curl_result
+    end
+  end
+
+  setup do
+    Chimp.connect("key-us1")
+  end
+
+  test "lists" do |chimp|
+    $curl_result = JSON.dump({ id: "12345" })
+
+    result = chimp.lists
+
+    assert_equal({ "id" => "12345" }, result)
+
+    curl = 'curl -X POST --data "apikey=key-us1" ' +
+           'https://us1.api.mailchimp.com/1.3/?method=lists'
+
+    assert_equal curl, $curl
+  end
+
+  test "subscribe" do |chimp|
+    $curl_result = "OK"
+
+    result = chimp.subscribe("foo@bar.com", "654321")
+
+    assert_equal("OK", result)
+
+    curl = 'curl -X POST --data "apikey=key-us1&email_address=foo%40bar.com' +
+           '&id=654321" https://us1.api.mailchimp.com/1.3/?method=listSubscribe'
+
+    assert_equal curl, $curl
+  end
+end
